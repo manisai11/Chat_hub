7100b5867b0b4944f77e681ed87ca05a46fb93fe
Mani Sai <151740753+manisai11@users.noreply.github.com>
2025-04-05 21:17:45 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,13 +1,10 @@
 from pymongo import MongoClient
-from dotenv import load_dotenv
 import os
-
-load_dotenv("priv.env")  # Load environment variables from .env file
 
 MONGO_URI = os.getenv("MONGO_URI")
 client = MongoClient(MONGO_URI)
 db = client["chatuserinfo"]
 
-users_collection = db["userdata"]  # Ensure this matches your database structure
-otp_collection = db["otp_storage"]  # Define the OTP collection
-messages_collection = db["messages"]  # Define the messages collection
+users_collection = db["userdata"]
+otp_collection = db["otp_storage"]
+messages_collection = db["messages"]
